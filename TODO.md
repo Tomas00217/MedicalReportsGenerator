89e34ebce32fbb0ab3e2a81458eeac993cceafc8
Tomas Stucka <tomas.stucka@bluesoft.cz>
2023-01-31 16:38:23 +0100

    fixed tests

--- TODO.md
+++ TODO.md
@@ -2,4 +2,3 @@
 1. Improve error logging
 2. Pisat testy pre parser
 3. Zistit moznosti spajania JSONov -> mat 1 DEFAULT a jazykove ktore to overridnu
-4. Preskumat https://python-poetry.org/
--- medicalrecordgenerator/tests/test.py
+++ medicalrecordgenerator/tests/test.py
@@ -1,169 +1,190 @@
 import unittest
 
-import medicalrecordgenerator.app.parser as parser
+from app.parser import Parser
 
 
 class TestParser(unittest.TestCase):
+    parser = Parser({})
+
     def test_and_true(self):
         test_json = {"type": "AND", "conditions": [{"type": "EXISTENCE",
-                                                    "scope": "a",
+                                                    "scope": "scope.a",
                                                     "value": True},
                                                    {"type": "EXISTENCE",
-                                                    "scope": "b",
+                                                    "scope": "scope.b",
                                                     "value": True}]}
-        data = {"b": "test", "a": 4}
-        self.assertTrue(parser.parse_and(test_json, data))
+        data = {"scope": {"b": "test", "a": 4}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_and(test_json))
 
     def test_and_false(self):
         test_json = {"type": "AND", "conditions": [{"type": "EXISTENCE",
-                                                    "scope": "a",
+                                                    "scope": "scope.a",
                                                     "value": True},
                                                    {"type": "EXISTENCE",
-                                                    "scope": "b",
+                                                    "scope": "scope.b",
                                                     "value": False}]}
-        data = {"b": "test", "a": 4}
-        self.assertFalse(parser.parse_and(test_json, data))
+        data = {"scope": {"b": "test", "a": 4}}
+        self.parser.data = data
+        self.assertFalse(self.parser.parse_and(test_json))
 
     def test_or_true(self):
         test_json = {"type": "OR", "conditions": [{"type": "EXISTENCE",
-                                                   "scope": "a",
+                                                   "scope": "scope.a",
                                                    "value": False},
                                                   {"type": "EXISTENCE",
-                                                   "scope": "b",
+                                                   "scope": "scope.b",
                                                    "value": True}]}
-        data = {"b": "test", "a": 4}
-        self.assertTrue(parser.parse_or(test_json, data))
+        data = {"scope": {"b": "test", "a": 4}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_or(test_json))
 
     def test_or_false(self):
         test_json = {"type": "OR", "conditions": [{"type": "EXISTENCE",
-                                                   "scope": "a",
+                                                   "scope": "scope.a",
                                                    "value": False},
                                                   {"type": "EXISTENCE",
-                                                   "scope": "b",
+                                                   "scope": "scope.b",
                                                    "value": False}]}
-        data = {"b": "test", "a": 4}
-        self.assertFalse(parser.parse_or(test_json, data))
+        data = {"scope": {"b": "test", "a": 4}}
+        self.parser.data = data
+        self.assertFalse(self.parser.parse_or(test_json))
 
     def test_not_true(self):
         test_json = {"type": "NOT", "conditions": [{"type": "EXISTENCE",
-                                                    "scope": "a",
+                                                    "scope": "scope.a",
                                                     "value": False},
                                                    {"type": "EXISTENCE",
-                                                    "scope": "b",
+                                                    "scope": "scope.b",
                                                     "value": False}]}
-        data = {"b": "test", "a": 4}
-        self.assertTrue(parser.parse_not(test_json, data))
+        data = {"scope": {"b": "test", "a": 4}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_not(test_json))
 
     def test_not_false(self):
         test_json = {"type": "NOT", "conditions": [{"type": "EXISTENCE",
-                                                    "scope": "a",
+                                                    "scope": "scope.a",
                                                     "value": True}]}
-        data = {"a": 4}
-        self.assertFalse(parser.parse_not(test_json, data))
+        data = {"scope": {"a": 4}}
+        self.parser.data = data
+        self.assertFalse(self.parser.parse_not(test_json))
 
     def test_value_num_1(self):
         test_json = {"type": "VALUE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": 4}
 
-        data = {"a": 4}
-        self.assertTrue(parser.parse_value(test_json, data))
+        data = {"scope": {"a": 4}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_value(test_json))
 
     def test_value_num_2(self):
         test_json = {"type": "VALUE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": 3}
 
-        data = {"a": 4}
-        self.assertFalse(parser.parse_value(test_json, data))
+        data = {"scope": {"a": 4}}
+        self.parser.data = data
+        self.assertFalse(self.parser.parse_value(test_json))
 
     def test_value_bool_1(self):
         test_json = {"type": "VALUE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": False}
 
-        data = {"a": False}
-        self.assertTrue(parser.parse_value(test_json, data))
+        data = {"scope": {"a": False}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_value(test_json))
 
     def test_value_bool_2(self):
         test_json = {"type": "VALUE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": True}
 
-        data = {"a": False}
-        self.assertFalse(parser.parse_value(test_json, data))
+        data = {"scope": {"a": False}}
+        self.parser.data = data
+        self.assertFalse(self.parser.parse_value(test_json))
 
     def test_existence_num_1(self):
         test_json = {"type": "EXISTENCE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": True}
 
-        data = {"a": 1}
-        self.assertTrue(parser.parse_existence(test_json, data))
+        data = {"scope": {"a": 1}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_existence(test_json))
 
     def test_existence_bool_1(self):
         test_json = {"type": "EXISTENCE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": True}
 
-        data = {"a": True}
-        self.assertTrue(parser.parse_existence(test_json, data))
+        data = {"scope": {"a": True}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_existence(test_json))
 
     def test_existence_str_1(self):
         test_json = {"type": "EXISTENCE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": True}
 
-        data = {"a": "aloha"}
-        self.assertTrue(parser.parse_existence(test_json, data))
+        data = {"scope": {"a": "aloha"}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_existence(test_json))
 
     def test_existence_str_2(self):
         test_json = {"type": "EXISTENCE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": False}
 
-        data = {"a": ""}
-        self.assertTrue(parser.parse_existence(test_json, data))
+        data = {"scope": {"a": ""}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_existence(test_json))
 
     def test_existence_str_3(self):
         test_json = {"type": "EXISTENCE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": False}
 
-        data = {"a": ""}
-        self.assertTrue(parser.parse_existence(test_json, data))
+        data = {"scope": {"a": ""}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_existence(test_json))
 
     def test_existence_str_4(self):
         test_json = {"type": "EXISTENCE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": False}
 
-        data = {"a": "aloha"}
-        self.assertFalse(parser.parse_existence(test_json, data))
+        data = {"scope": {"a": "aloha"}}
+        self.parser.data = data
+        self.assertFalse(self.parser.parse_existence(test_json))
 
     def test_existence_str_5(self):
         test_json = {"type": "EXISTENCE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": True}
 
-        data = {"a": ""}
-        self.assertFalse(parser.parse_existence(test_json, data))
+        data = {"scope": {"a": ""}}
+        self.parser.data = data
+        self.assertFalse(self.parser.parse_existence(test_json))
 
     def test_existence_none_1(self):
         test_json = {"type": "EXISTENCE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": False}
 
-        data = {"a": None}
-        self.assertTrue(parser.parse_existence(test_json, data))
+        data = {"scope": {"a": None}}
+        self.parser.data = data
+        self.assertTrue(self.parser.parse_existence(test_json))
 
     def test_existence_none_2(self):
         test_json = {"type": "EXISTENCE",
-                     "scope": "a",
+                     "scope": "scope.a",
                      "value": True}
 
-        data = {"a": None}
-        self.assertFalse(parser.parse_existence(test_json, data))
+        data = {"scope": {"a": None}}
+        self.parser.data = data
+        self.assertFalse(self.parser.parse_existence(test_json))
 
 
 if __name__ == '__main__':
